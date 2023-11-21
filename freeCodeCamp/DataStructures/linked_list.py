04fb7e57546d5112607f64f86a6604b73aa5c4db
Edveika <edvinasbureika@gmail.com>
2023-11-21 19:02:19 +0200

    Linked list structure, append/preappend data methods

--- freeCodeCamp/DataStructures/linked_list.py
+++ freeCodeCamp/DataStructures/linked_list.py
@@ -7,8 +7,42 @@ class Node:
         self.data = data
 
     # Returns a string that contains the data of the node
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "Node data: " + str(self.data)
     
-n1 = Node(1337)
-print(n1)
+class LinkedList:
+    head: Node  = None
+
+    def __init__(self):
+        self.head = None
+
+    # Returns true if linked list is empty
+    # Returns false if linked list is not empty
+    def is_empty(self) -> bool:
+        return self.head == None
+    
+    # Returns number of elements in the linked list
+    def size(self) -> int:
+        curr = self.head
+        size = 0
+
+        while curr:
+            size += 1
+            curr = curr.next
+
+        return size
+    
+    # Adds data at the beggining of the list
+    def preappend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Adds data to the end of the list
+    def append(self, data):
+        curr = self.head
+
+        while curr:
+            curr = curr.next
+
+        curr.data = data
