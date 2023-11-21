5564f3a8046323bf1cc8999290d59e8f921c36b9
Edveika <edvinasbureika@gmail.com>
2023-11-21 21:55:21 +0200

    Merge sort logic

--- freeCodeCamp/Algorithms/merge_sort.py
+++ freeCodeCamp/Algorithms/merge_sort.py
@@ -1,11 +1,38 @@
 
 # Splits a list into two
 def split(list):
-    pass
+    mid = len(list) // 2
+
+    left = list[:mid]
+    right = list[mid:]
+
+    return left, right
 
 # Merges left and right lists together
 def merge(left, right):
-    pass
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    # If there are still some elements left in the left half, add them to the list
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    
+    # Same to the right half
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
 
 # Divide: divide list into multiple lists until each list contains 1 elements
 # Conquer: sort everything in ascending order
@@ -21,3 +48,8 @@ def merge_sort(list):
     right = merge_sort(right)
 
     return merge(left, right)
+
+
+array = [213,54,23,76,3,34,6,7,908,12,213]
+
+print(merge_sort(array))
